ee625629aa6786eddb43e80169ff3c6ac60e964b
Urieljavier <urieljavieraiz@gmail.com>
2020-01-13 13:29:45 +0100

    Updates

---
+++ precache-manifest.728f9e1588565ed32a26faa54bbfe436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e459934a1aaaa8581979ef32e230f35",
+    "url": "/react-calculadora/index.html"
+  },
+  {
+    "revision": "b997c9ca2cc50775cbb0",
+    "url": "/react-calculadora/static/css/main.e47c6c5b.chunk.css"
+  },
+  {
+    "revision": "bb707dc5ef44cead46dc",
+    "url": "/react-calculadora/static/js/2.3e5b4ba4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculadora/static/js/2.3e5b4ba4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b997c9ca2cc50775cbb0",
+    "url": "/react-calculadora/static/js/main.ce1eee0b.chunk.js"
+  },
+  {
+    "revision": "4c31972c503895bf1f72",
+    "url": "/react-calculadora/static/js/runtime-main.a5cc4613.js"
+  }
+]);
